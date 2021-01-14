91bf6c0dc62e0a0f24fcfa26b49f0146da3e0af8
Marina-sky <75041628+Marina-sky@users.noreply.github.com>
2021-01-14 19:17:24 +0100

    Added forecast

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 // Day and time
 function formatDate(timestamp) {
   let date = new Date(timestamp);
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-    if (minutes < 10) {
-    minutes = `0${minutes}`;
-    }
+
   let days = [
   "Sunday",
   "Monday",
@@ -16,7 +12,17 @@ function formatDate(timestamp) {
   "Saturday"
   ];
   let day = days[date.getDay()];
-  return `${day}, ${hours}:${minutes}`;
+  return `${day}, ${formatHours(timestamp)}`;
+}
+
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+    if (minutes < 10) {
+    minutes = `0${minutes}`;
+    }
+  return `${hours}:${minutes}`;
 }
 
 // Show city and current weather
@@ -37,10 +43,32 @@ function showCurrentWeather(response) {
   document.querySelector("#condition-icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
+function showForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    let forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+    <div class="col">
+      <p>
+        ${formatHours(forecast.dt * 1000)} <br />
+        <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" />
+        <strong>${Math.round(forecast.main.temp_max)}</strong> ${Math.round(forecast.main.temp_min)}
+      </p>
+    </div>
+  `;
+  } 
+}
+
 function searchCity(cityInput) {
   let apiKey = "ba32fdef5d635b26f61cd641f6ab56c1";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showCurrentWeather);
+
+  apiUrl = `api.openweathermap.org/data/2.5/forecast?q=${cityInput}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
 }
 
 function handleSubmit(event) {
